17b647f405ff269de05f9f3100bac0591fce2586
Pedrinhoou <pedropaulolemes007@gmail.com>
2024-08-13 08:17:50 -0400

    fix

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ const img = document.querySelector("#profile img");
    if (html.classList.contains("light")) {
     // Se tiver light mode, adicionar a imagem light
       img.setAttribute("src", "./assets/avatar-light.png");
+       img.setAttribute(
+         "alt",
+         "foto de banguela do filme como treinar seu dragao"
+       );
    } else {
       // Se tiver dark mode, adicionar a imagem dark 
       img.setAttribute("src", "./assets/avatar.webp");
